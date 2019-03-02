db59cb5c98c20372987e8eae7bc15673ac871998
JWHAPO <kim2101714@gmail.com>
2019-03-03 00:36:37 +0900

    clear Main

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,58 +4,10 @@ package main;/*
  * Description :
  */
 
-import questions.Question14;
-import questions.Question15;
-import questions.Question17;
-import questions.SortedSearch;
-
 public class Main {
 
     public static void main(String[] args){
 
-
-        System.out.println("SortedSearch.countNumbers(new int[]{1,2,3}, 4): "
-                +SortedSearch.countNumbers(new int[]{1,2,3}, 4));
-//        Question17 question17 = new Question17();
-//        System.out.println("solution:"+question17.solution(1000000));
-
-//        Question15 question15 = new Question15();
-//        System.out.println(question15.solution("12345"));
-//        Question14 question14 = new Question14();
-//        System.out.println("answer:"+question14.solution(new int[]{1,1,1,1,1},3));
-
-//        List<String> list = new ArrayList<>();
-//        list.add("AAA");
-
-        //Question 13
-//        Question13 question13 = new Question13();
-//        question13.solution("Zbcdefg");
-//        //Question12
-//        Question12 question12 = new Question12();
-//        String[] arr = new String[]{"abce", "abcd", "cdx"};
-//        question12.solution(arr,2);
-
-//        //Question11
-//        Question11 question11 = new Question11();
-//        System.out.println("result:"+question11.solution(new int[]{6, 10, 2}));
-
-
-
-
-        //UserInput
-//        String[] names1 = new String[] {"Ava", "Emma", "Olivia","Mr.Kim"};
-//        String[] names2 = new String[] {"Olivia", "Sophia", "Emma","Mr.Kim"};
-//
-//        System.out.println(String.join(",", MergeNames.uniqueNames(names1,names2)));
-
-
-        //questions\alert_service
-//        AlertService alertService = new AlertService(new MapAlertDAO());
-//        System.out.println(alertService.raiseAlert());
-//        System.out.println(alertService.getAlertTime(alertService.raiseAlert()));
-
     }
-
-
 
 }
